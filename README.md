0697a43b6f17d6cdcc8e3c489136f545005d7be7
BirtukanK <birtukankuma1113@gmail.com>
2023-04-05 14:49:38 +0300

    file structure

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# Nutrition_app
+# HooBank - Modern UI/UX website using React.js & Tailwind CSS
 
-A website to support and guide clients in making better food and lifestyle choices to improve nutrition and health
+![HooBank](https://i.ibb.co/BK1Hn0x/Screenshot-2022-08-08-at-4-05-48-PM.png)
+
+
+### Showcase your dev skills with practical experience and land the coding career of your dreams
+💻 JS Mastery Pro - https://jsmastery.pro/youtube
+✅ A special YOUTUBE discount code is automatically applied!
+
+📙 Get the Ultimate Frontend & Backend Development Roadmaps, a Complete JavaScript Cheatsheet, Portfolio Tips, and more - https://www.jsmastery.pro/links
---
+++ src/components/Hero.jsx
@@ -0,0 +1,43 @@
+import styles from "../style";
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
+
+const Hero = () => {
+  return (
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            Nutrition, <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">Health and</span>{" "}
+          </h1>
+          
+        </div>
+
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+          wellness consultant
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Our website will show you a range of nutrition and wellness products that are formulated with the highest quality natural ingredients. Our products are aimed at helping you achieve your weight loss goals and other health goals, such as increasing energy and vitality.
+        </p>
+      </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+        {/* gradient start */}
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+        {/* gradient end */}
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
