167a8e27b6e10ccc405baee3da28dca4c6ce1584
Birtukan Kuma <birtukankuma1113@gmail.com>
2024-07-30 21:25:08 +0300

    update package

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import { logo } from "../assets";
 import { footerLinks, socialMedia } from "../constants";
 
 const Footer = () => (
-  <section id="contact" className={`${styles.flexCenter} ${styles.paddingY} flex-col bg-black-gradient-2 w-[1280px]`}>
+  <section id="contact" className={`${styles.flexCenter} ${styles.paddingY} flex-col bg-white-gradient-2 w-[1200px]`}>
     <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
       <div className="flex-[1] flex flex-col justify-start mr-10 ">
-      <h1 className={`font-poppins font-normal text-[20px] text-white flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] text-gradient`}>YiHA</h1>
-        <p className={`${styles.paragraph} mt-4 max-w-[312px] text-white`}>
+      <h1 className={`font-poppins font-normal text-[20px] text-white flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-black ss:leading-[100.8px] leading-[75px] text-gradient`}>YiHA</h1>
+        <p className={`${styles.paragraph} mt-4 max-w-[312px] text-black`}>
         Phone Number: +251973095959
         <br />
         <br /> Email: yisihak1girma@gmail.com
@@ -17,14 +17,14 @@ const Footer = () => (
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
         {footerLinks.map((footerlink) => (
           <div key={footerlink.title} className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}>
-            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
+            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-black">
               {footerlink.title}
             </h4>
             <ul className="list-none mt-4">
               {footerlink.links.map((link, index) => (
                 <li
                   key={link.name}
-                  className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
+                  className={`font-poppins font-normal text-[16px] leading-[24px] text-black hover:text-secondary cursor-pointer ${
                     index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
                   }`}
                 >
@@ -38,7 +38,7 @@ const Footer = () => (
     </div>
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
-      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
+      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-black">
         Copyright Ⓒ 2024 YiHA. All Rights Reserved.
       </p>
 
